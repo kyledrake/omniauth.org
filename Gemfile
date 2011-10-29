dd70e00fdc92b117ec9391ffe0d063804c062f80
Michael Bleigh <michael@intridea.com>
2011-10-29 13:09:12 -0400

    Work towards live examples.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'compass'
+gem 'redis'
 
 gem 'omniauth',          '~> 1.0.0.rc2'
 gem 'omniauth-github',   '~> 1.0.0.rc2', :git => 'git://github.com/intridea/omniauth-github.git'
--- application.rb
+++ application.rb
@@ -1,14 +1,54 @@
+$:.unshift File.dirname(__FILE__) + '/lib'
 ENV['RACK_ENV'] ||= 'development'
 
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV']
 
+require 'redis'
+require 'openid-redis-store'
+
+if ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  $redis = Redis.new
+end
+
 use Rack::Session::Cookie
 use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  provider :open_id
+  provider :open_id, :store => OpenID::Store::Redis.new($redis, 'openid:')
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+end
+
+OMNIAUTH_STRATEGIES = YAML.load_file(File.dirname(__FILE__) + '/providers.yml').inject([]) do |arr, (provider, hash)|
+  arr << Hashie::Mash.new(hash.merge(slug: provider))
+  arr
+end
+
+helpers do
+  def htmlize_hash(hash, nested = false)
+    output = "<table class='hash'>"
+    hash.each_pair do |key, value|
+      output << "<tr><th>#{key}</th><td>"
+      case value
+      when Hash
+        if nested
+          output << "<span class='object'>Hash</span>"
+        else
+          output << htmlize_hash(value, true)
+        end
+      when String
+        output << value
+      else
+        output << "<span class='object'>#{value.class.to_s}</span>"
+      end
+      output << "</td></tr>"
+    end
+    output << "</table>"
+    output
+  end
 end
 
 get '/' do
